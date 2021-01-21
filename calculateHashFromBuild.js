05a6282a1cfebcddea0d79c65d05d39c9899e82f
Rasmusson <emma.rasmusson@nav.no>
2021-01-21 19:16:46 +0100

    La till SHA384 med

--- calculateHashFromBuild.js
+++ calculateHashFromBuild.js
@@ -3,23 +3,27 @@ const fs = require('fs');
 const path = require('path');
 const pkg = require('./package.json');
 
-console.log('------------------ Generating SHA256 hash -----------------');
-
 let fileToGenerateHash;
 process.argv.forEach((val, index) => {
   if(index == 2) fileToGenerateHash = val;
 });
 
+console.log(`-------------------------------  Generated hashes  --------------------------------`);
+
 const pathToFile = path.join(__dirname, '/build', `/${pkg.version}`, fileToGenerateHash);
-const hash = crypto.createHash('sha256');
+const hashSHA256 = crypto.createHash('sha256');
+const hashSHA384 = crypto.createHash('sha384');
 
 const stream = fs.ReadStream(pathToFile);
 stream.on('data', (data) => {
-  hash.update(data);
+  hashSHA256.update(data);
+  hashSHA384.update(data);
 });
 
 stream.on('end', () => {
-  const base64EncodedHash = hash.digest('base64');
-  console.log(`${fileToGenerateHash}: ${base64EncodedHash}`);
-  console.log('-----------------------------------------------------------');
+  const base64EncodedSHA256Hash = hashSHA256.digest('base64');
+  const base64EncodedSHA384Hash = hashSHA384.digest('base64');
+  console.log(`SHA256 ${fileToGenerateHash}: ${base64EncodedSHA256Hash}`);
+  console.log(`SHA384 ${fileToGenerateHash}: ${base64EncodedSHA384Hash}`);
+  console.log('-----------------------------------------------------------------------------------');
 });
