bbce42433143840edaecc178fa74e0ad3e14fb75
Rasmusson <emma.rasmusson@nav.no>
2021-02-02 11:54:49 +0100

    La til config för lokal utvikling och toggle mellan komponenter

---
+++ localDevConfig.js
@@ -0,0 +1,6 @@
+export const localDevConfig = {
+  lokalUtvikling: true,
+  visVilkarKroniskSyktBarn: true,
+  visVilkarMidlertidigAlene: false,
+  visKorrigerePerioder: true
+};
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import renderers from './util/renderers';
 import ContainerContract from './types/ContainerContract';
+import {localDevConfig} from '../localDevConfig.js'
 import {vilkarKroniskSyktBarn, vilkarMidlertidigAlene, korrigerePerioder} from './mock/mockedContainerData';
 
 (window as any).renderMicrofrontendOmsorgsdagerApp = async (appId, data: ContainerContract) => {
-    const { renderAppInSuccessfulState } = renderers;
-    renderAppInSuccessfulState(appId, data);
-};
+  const {renderAppInSuccessfulState} = renderers;
+  if (!localDevConfig.lokalUtvikling) renderAppInSuccessfulState(appId, data);
+  else {
+    if (localDevConfig.visVilkarKroniskSyktBarn) renderAppInSuccessfulState(appId, vilkarKroniskSyktBarn);
+    else if (localDevConfig.visVilkarMidlertidigAlene) renderAppInSuccessfulState(appId, vilkarMidlertidigAlene);
+    else if (localDevConfig.visKorrigerePerioder) renderAppInSuccessfulState(appId, korrigerePerioder);
+    else renderAppInSuccessfulState(appId, data);
+  }
+}
