33317d3b0e7e3276b508f1fdcb921c4ded6d25da
TomasEng <tomas.engebretsen@nav.no>
2021-02-04 08:30:50 +0100

    TSF-399: Utvider mikrofrontend for korrigering av perioder (#6)

--- src/ui/components/korrigere-perioder/KorrigerePerioder.tsx
+++ src/ui/components/korrigere-perioder/KorrigerePerioder.tsx
@@ -1,12 +1,39 @@
 import {AlertStripeAdvarsel} from "nav-frontend-alertstriper";
-import React from 'react';
+import {Radio, RadioGruppe, Textarea} from "nav-frontend-skjema";
+import React, {useState} from 'react';
 import styles from './korrigerePerioder.less';
 
 const KorrigerePerioder = () => {
+
+    const [innvilgedePerioder, endreInnvilgedePerioder] = useState<"alle" | "ingen">("ingen");
+    const [begrunnelse, endreBegrunnelse] = useState<string>("");
+
+    // TODO: Avventer avklaringer på hvordan denne skal se ut
+
     return <div className={styles.korrigerePerioder}>
         <AlertStripeAdvarsel className={styles.varselstripe}>
-            Det finnes avslåtte perioder. Korriger eller legg til rammemeldinger i Infotrygd, eller fortsett dersom avslåtte perioder er korrekte.
+            Se på dokumentasjonen og vurder om den dekker alle fraværsperioder.
         </AlertStripeAdvarsel>
+        <RadioGruppe legend="Er periodene innvilget pga. særlige smittevernshensyn eller stengt barnehage/skole/sfo?">
+            <Radio
+                label="Ja"
+                name="alle"
+                checked={innvilgedePerioder === "alle"}
+                onChange={e => e.target.value && endreInnvilgedePerioder("alle")}
+            />
+            <Radio
+                label="Nei"
+                name="ingen"
+                checked={innvilgedePerioder === "ingen"}
+                onChange={e => e.target.value && endreInnvilgedePerioder("ingen")}
+            />
+        </RadioGruppe>
+        <Textarea
+            label="Begrunnelse"
+            onChange={e => endreBegrunnelse(e.target.value)}
+            value={begrunnelse}
+            maxLength={0}
+        />
     </div>;
 };
 export default KorrigerePerioder;
