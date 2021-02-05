92402df13adacb8b7cded098027f5ac238c26cfa
eras-dev <emma.rasmusson@nav.no>
2021-02-05 11:26:48 +0100

    Felhantering i midlertidig alene

--- src/ui/components/vilkar-midlertidig-alene/VilkarMidlertidigAlene.tsx
+++ src/ui/components/vilkar-midlertidig-alene/VilkarMidlertidigAlene.tsx
@@ -2,49 +2,61 @@ import AlertStripe, {AlertStripeAdvarsel} from "nav-frontend-alertstriper";
 import {Datepicker} from 'nav-datovelger';
 import {Hovedknapp} from "nav-frontend-knapper";
 import OpplysningerFraSoknad from "./opplysninger-fra-soknad/opplysningerFraSoknad";
-import {Radio, RadioGruppe, Textarea} from "nav-frontend-skjema";
+import {Radio, RadioGruppe, Textarea, SkjemaGruppe} from "nav-frontend-skjema";
 import React, {useState} from 'react';
 import styles from './vilkarMidlertidigAlene.less'
 import {VilkarMidlertidigAleneProps} from "../../../types/VilkarMidlertidigAleneProps";
 
+interface Feilmeldinger {
+  begrunnelse: boolean;
+  dato: {
+    til: boolean;
+    fra: boolean;
+  }
+}
+
 const VilkarMidlertidigAlene: React.FunctionComponent<VilkarMidlertidigAleneProps> = props => {
 
   const [begrunnelse, endreBegrunnelse] = useState('');
-  const [feilmedling, endreFeilmedling] = useState('');
   const [fraDato, endreFraDato] = useState('DD.MM.ÅÅÅÅ');
   const [tilDato, endreTilDato] = useState('DD.MM.ÅÅÅÅ');
-  const [vilkarOppfylt, endreVilkarOppfylt] = useState(false);
+  const [visFeilmedlinger, endreVisFeilmedlinger] = useState<boolean>(false);
+  const [vilkarOppfylt, endreVilkarOppfylt] = useState(true);
 
-  const sjekkHvisVurderingErKomplett = () => {
-    if ((tilDato == 'DD.MM.ÅÅÅÅ' || fraDato == 'DD.MM.ÅÅÅÅ') && vilkarOppfylt) {
-      endreFeilmedling('Mangler i hvilken periode vedtaket er gyldig.');
-    } else if (begrunnelse.length === 0) {
-      endreFeilmedling('Mangler begrunnelse.');
-    } else {
-      endreFeilmedling('');
-      const dato = {
-        til: vilkarOppfylt ? tilDato : '',
-        fra: vilkarOppfylt ? fraDato : ''
-      };
-      props.onSubmit(vilkarOppfylt, dato, begrunnelse);
+  const feilmedlinger: Feilmeldinger = {
+    begrunnelse: begrunnelse.length === 0,
+    dato: {
+      til: tilDato == 'DD.MM.ÅÅÅÅ' && vilkarOppfylt,
+      fra: fraDato == 'DD.MM.ÅÅÅÅ' && vilkarOppfylt
     }
-  }
+  };
+
+  const vurderingKomplett = !feilmedlinger.begrunnelse && !feilmedlinger.dato.til && !feilmedlinger.dato.fra;
+  const visFeilmedlingForDato = visFeilmedlinger && feilmedlinger.dato.fra && feilmedlinger.dato.til && 'Mangler dato.'
+    || visFeilmedlinger && feilmedlinger.dato.til && !feilmedlinger.dato.fra && 'Manger til dato.'
+    || visFeilmedlinger && feilmedlinger.dato.fra && !feilmedlinger.dato.til && 'Mangler fra dato.';
+
+  const sjekkHvisVurderingErKomplett = () => vurderingKomplett ?
+    props.onSubmit(vilkarOppfylt, {til: vilkarOppfylt ? tilDato : '', fra: vilkarOppfylt ? fraDato : ''}, begrunnelse) :
+    endreVisFeilmedlinger(true)
 
   return (
     <div className={styles.vilkarMidlerTidigAlene}>
       <AlertStripe type="advarsel">Vurder om vilkår om aleneomsorg er oppfylt.</AlertStripe>
-      {feilmedling !== '' && <AlertStripe type="feil">{feilmedling}</AlertStripe>}
 
       <OpplysningerFraSoknad {...props.soknedsopplysninger}/>
 
       <RadioGruppe className={styles.radioButtons} legend="Er vilkårene om aleneomsorg oppfylt?">
-        <Radio label={"Ja"} checked={vilkarOppfylt} onChange={() => endreVilkarOppfylt(true)} name="vilkarAleneomsorg"/>
+        <Radio label={"Ja"} checked={vilkarOppfylt} onChange={() => endreVilkarOppfylt(true)}
+               name="vilkarAleneomsorg"/>
         <Radio label={"Nei"} checked={!vilkarOppfylt} onChange={() => endreVilkarOppfylt(false)}
                name="vilkarAleneomsorg"/>
       </RadioGruppe>
 
-      {vilkarOppfylt && <div className={styles.gyldigVedtaksPeriode}>
-        <span className={styles.gyldigVedtaksPeriodeOverskrift}>I hvilken periode er vedtaket gyldig?</span>
+      {vilkarOppfylt &&
+      <SkjemaGruppe className={styles.gyldigVedtaksPeriode}
+                    legend={'I hvilken periode er vedtaket gyldig?'}
+                    feil={visFeilmedlingForDato}>
         <div>
           <span className={styles.gyldigVedtaksPeriodeTilFra}>Fra</span>
           <Datepicker onChange={endreFraDato} value={fraDato}/>
@@ -53,11 +65,13 @@ const VilkarMidlertidigAlene: React.FunctionComponent<VilkarMidlertidigAleneProp
           <span className={styles.gyldigVedtaksPeriodeTilFra}>Til</span>
           <Datepicker onChange={endreTilDato} value={tilDato}/>
         </div>
-      </div>}
+      </SkjemaGruppe>
+      }
 
       <Textarea label="Begrunnelse"
                 value={begrunnelse}
                 onChange={e => endreBegrunnelse(e.target.value)}
+                feil={visFeilmedlinger && feilmedlinger.begrunnelse && 'Begrunnelse må oppgis.'}
       />
 
       <Hovedknapp className={styles.bekreftKnapp} onClick={sjekkHvisVurderingErKomplett}>
@@ -66,4 +80,5 @@ const VilkarMidlertidigAlene: React.FunctionComponent<VilkarMidlertidigAleneProp
     </div>
   )
 }
+
 export default VilkarMidlertidigAlene;
