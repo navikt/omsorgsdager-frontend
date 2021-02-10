c2fab355a4f881f21a36747aa32c59a2974abbaa
eras-dev <emma.rasmusson@nav.no>
2021-02-10 10:03:24 +0100

    Fixet indentering

--- src/ui/components/vilkar-midlertidig-alene/VilkarMidlertidigAlene.tsx
+++ src/ui/components/vilkar-midlertidig-alene/VilkarMidlertidigAlene.tsx
@@ -32,80 +32,77 @@ const VilkarMidlertidigAlene: React.FunctionComponent<VilkarMidlertidigAleneProp
   onSubmit,
   lesemodus
 }) => {
-    const [begrunnelse, endreBegrunnelse] = useState('');
-    const [fraDato, endreFraDato] = useState('DD.MM.ÅÅÅÅ');
-    const [tilDato, endreTilDato] = useState('DD.MM.ÅÅÅÅ');
-    const [visFeilmedlinger, endreVisFeilmedlinger] = useState<boolean>(false);
-    const [vilkarOppfylt, endreVilkarOppfylt] = useState(true);
+  const [begrunnelse, endreBegrunnelse] = useState('');
+  const [fraDato, endreFraDato] = useState('DD.MM.ÅÅÅÅ');
+  const [tilDato, endreTilDato] = useState('DD.MM.ÅÅÅÅ');
+  const [visFeilmedlinger, endreVisFeilmedlinger] = useState<boolean>(false);
+  const [vilkarOppfylt, endreVilkarOppfylt] = useState(true);
 
-    const feilmedlinger: Feilmeldinger = {
-      begrunnelse: begrunnelse.length === 0,
-      dato: {
-        til: tilDato == 'DD.MM.ÅÅÅÅ' && vilkarOppfylt,
-        fra: fraDato == 'DD.MM.ÅÅÅÅ' && vilkarOppfylt
-      }
-    };
+  const feilmedlinger: Feilmeldinger = {
+    begrunnelse: begrunnelse.length === 0,
+    dato: {
+      til: tilDato == 'DD.MM.ÅÅÅÅ' && vilkarOppfylt,
+      fra: fraDato == 'DD.MM.ÅÅÅÅ' && vilkarOppfylt
+    }
+  };
 
-    const vurderingKomplett = !feilmedlinger.begrunnelse && !feilmedlinger.dato.til && !feilmedlinger.dato.fra;
-    const visFeilmedlingForDato = visFeilmedlinger && feilmedlinger.dato.fra && feilmedlinger.dato.til && 'Mangler dato.'
-      || visFeilmedlinger && feilmedlinger.dato.til && !feilmedlinger.dato.fra && 'Manger til dato.'
-      || visFeilmedlinger && feilmedlinger.dato.fra && !feilmedlinger.dato.til && 'Mangler fra dato.';
+  const vurderingKomplett = !feilmedlinger.begrunnelse && !feilmedlinger.dato.til && !feilmedlinger.dato.fra;
+  const visFeilmedlingForDato = visFeilmedlinger && feilmedlinger.dato.fra && feilmedlinger.dato.til && 'Mangler dato.'
+    || visFeilmedlinger && feilmedlinger.dato.til && !feilmedlinger.dato.fra && 'Manger til dato.'
+    || visFeilmedlinger && feilmedlinger.dato.fra && !feilmedlinger.dato.til && 'Mangler fra dato.';
 
-    const sjekkHvisVurderingErKomplett = () => vurderingKomplett ?
-      onSubmit(vilkarOppfylt, {til: vilkarOppfylt ? tilDato : '', fra: vilkarOppfylt ? fraDato : ''}, begrunnelse) :
-      endreVisFeilmedlinger(true);
+  const sjekkHvisVurderingErKomplett = () => vurderingKomplett ?
+    onSubmit(vilkarOppfylt, {til: vilkarOppfylt ? tilDato : '', fra: vilkarOppfylt ? fraDato : ''}, begrunnelse) :
+    endreVisFeilmedlinger(true);
 
-    return (
-      <div className={classNames(styles.vilkarMidlerTidigAlene, lesemodus && styleLesemodus.lesemodusboks)}>
-        {lesemodus ?
-          <p><b>Behandlet aksjonspunkt:</b> Vurder om vilkår om midlertidig alene om omsorgen er oppfylt.</p> :
-          <AlertStripe type="advarsel">Vurder om vilkår om aleneomsorg er oppfylt.</AlertStripe>
-        }
+  return (
+    <div className={classNames(styles.vilkarMidlerTidigAlene, lesemodus && styleLesemodus.lesemodusboks)}>
+      {lesemodus ?
+        <p><b>Behandlet aksjonspunkt:</b> Vurder om vilkår om midlertidig alene om omsorgen er oppfylt.</p> :
+        <AlertStripe type="advarsel">Vurder om vilkår om aleneomsorg er oppfylt.</AlertStripe>}
 
-        <OpplysningerFraSoknad {...soknedsopplysninger}/>
-        {lesemodus && <OpplysningerFraVedtak {...opplysningerFraVedtak}/>}
+      <OpplysningerFraSoknad {...soknedsopplysninger}/>
+      {lesemodus && <OpplysningerFraVedtak {...opplysningerFraVedtak}/>}
 
-        {!lesemodus &&
-        <RadioGruppe className={styles.radioButtons} legend="Er vilkårene om aleneomsorg oppfylt?">
-          <Radio label={"Ja"}
-                 checked={vilkarOppfylt}
-                 onChange={() => endreVilkarOppfylt(true)}
-                 name="vilkarAleneomsorg"/>
-          <Radio label={"Nei"}
-                 checked={!vilkarOppfylt}
-                 onChange={() => endreVilkarOppfylt(false)}
-                 name="vilkarAleneomsorg"/>
-        </RadioGruppe>
-        }
+      {!lesemodus &&
+      <RadioGruppe className={styles.radioButtons} legend="Er vilkårene om aleneomsorg oppfylt?">
+        <Radio label={"Ja"}
+               checked={vilkarOppfylt}
+               onChange={() => endreVilkarOppfylt(true)}
+               name="vilkarAleneomsorg"/>
+        <Radio label={"Nei"}
+               checked={!vilkarOppfylt}
+               onChange={() => endreVilkarOppfylt(false)}
+               name="vilkarAleneomsorg"/>
+      </RadioGruppe>}
 
-        {!lesemodus && vilkarOppfylt &&
-        <SkjemaGruppe className={styles.gyldigVedtaksPeriode}
-                      legend={'I hvilken periode er vedtaket gyldig?'}
-                      feil={visFeilmedlingForDato}>
-          <div>
-            <span className={styles.gyldigVedtaksPeriodeTilFra}>Fra</span>
-            <Datepicker onChange={endreFraDato} value={fraDato}/>
-          </div>
-          <div>
-            <span className={styles.gyldigVedtaksPeriodeTilFra}>Til</span>
-            <Datepicker onChange={endreTilDato} value={tilDato}/>
-          </div>
-        </SkjemaGruppe>}
+      {!lesemodus && vilkarOppfylt &&
+      <SkjemaGruppe className={styles.gyldigVedtaksPeriode}
+                    legend={'I hvilken periode er vedtaket gyldig?'}
+                    feil={visFeilmedlingForDato}>
+        <div>
+          <span className={styles.gyldigVedtaksPeriodeTilFra}>Fra</span>
+          <Datepicker onChange={endreFraDato} value={fraDato}/>
+        </div>
+        <div>
+          <span className={styles.gyldigVedtaksPeriodeTilFra}>Til</span>
+          <Datepicker onChange={endreTilDato} value={tilDato}/>
+        </div>
+      </SkjemaGruppe>}
 
-        {!lesemodus &&
-        <>
-          <Textarea label="Begrunnelse"
-                    value={begrunnelse}
-                    onChange={e => endreBegrunnelse(e.target.value)}
-                    feil={visFeilmedlinger && feilmedlinger.begrunnelse && 'Begrunnelse må oppgis.'}
-          />
-          <Hovedknapp className={styles.bekreftKnapp} onClick={sjekkHvisVurderingErKomplett}>
-            Bekreft og fortsett
-          </Hovedknapp>
-        </>}
-      </div>
-    );
-  }
-;
+      {!lesemodus &&
+      <>
+        <Textarea label="Begrunnelse"
+                  value={begrunnelse}
+                  onChange={e => endreBegrunnelse(e.target.value)}
+                  feil={visFeilmedlinger && feilmedlinger.begrunnelse && 'Begrunnelse må oppgis.'}
+        />
+        <Hovedknapp className={styles.bekreftKnapp} onClick={sjekkHvisVurderingErKomplett}>
+          Bekreft og fortsett
+        </Hovedknapp>
+      </>}
+    </div>
+  );
+};
 
 export default VilkarMidlertidigAlene;
