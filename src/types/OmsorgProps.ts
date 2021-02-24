c5396453cea27338ea6c6300c38701d430b68bf7
TomasEng <tomas.engebretsen@nav.no>
2021-02-24 12:13:56 +0100

    Oppdaterer omsorgsbildet (#28)

--- src/types/OmsorgProps.ts
+++ src/types/OmsorgProps.ts
@@ -1,4 +1,5 @@
 export interface OmsorgProps {
   lesemodus?: boolean;
   harOmsorgen?: boolean;
+  barnetsFnr: string;
 }
--- src/ui/components/omsorg/Omsorg.tsx
+++ src/ui/components/omsorg/Omsorg.tsx
@@ -13,17 +13,23 @@ const Omsorg: React.FunctionComponent<OmsorgProps> = props => {
 
   const tekst = {
     instruksjon: 'Barnet er ikke registrert på samme adresse som søker. Vurder om søkeren har omsorgen for barnet.',
+    opplysningerFraSoknaden: 'Opplysninger fra søknaden:',
+    sokersBarn: 'Søkers barn:',
     sporsmalHarOmsorgen: 'Har søker omsorgen for barnet?',
-    begrunnelse: 'Begrunnelse'
+    begrunnelse: 'Begrunn om søker har omsorgen for barnet'
   };
 
   if (props.lesemodus) {
-    return <div className={styleLesemodus.lesemodusboks}>
+    return <div className={`${styleLesemodus.lesemodusboks} ${styles.omsorg}`}>
       <p><b>Behandlet aksjonspunkt:</b> {tekst.instruksjon}</p>
-      <p className={styleLesemodus.label}>{tekst.sporsmalHarOmsorgen}</p>
-      <p>{harOmsorgen ? 'Ja' : 'Nei'}</p>
+      <p>{tekst.opplysningerFraSoknaden}</p>
+      <p className={styleLesemodus.label}>{tekst.sokersBarn}</p>
+      <p>{props.barnetsFnr}</p>
+      <hr/>
       <p className={styleLesemodus.label}>{tekst.begrunnelse}</p>
       <p className={styleLesemodus.fritekst}>{begrunnelse}</p>
+      <p className={styleLesemodus.label}>{tekst.sporsmalHarOmsorgen}</p>
+      <p>{harOmsorgen ? 'Ja' : 'Nei'}</p>
     </div>;
   }
 
@@ -31,6 +37,16 @@ const Omsorg: React.FunctionComponent<OmsorgProps> = props => {
 
   return <div className={styles.omsorg}>
     <AlertStripeAdvarsel className={styles.varselstripe}>{tekst.instruksjon}</AlertStripeAdvarsel>
+    <p>{tekst.opplysningerFraSoknaden}</p>
+    <p className={styleLesemodus.label}>{tekst.sokersBarn}</p>
+    <p>{props.barnetsFnr}</p>
+    <hr/>
+    <Textarea
+      label={tekst.begrunnelse}
+      value={begrunnelse}
+      onChange={e => endreBegrunnelse(e.target.value)}
+      maxLength={0}
+    />
     <RadioGruppe legend={tekst.sporsmalHarOmsorgen} className={styles.harOmsorgen}>
       <Radio
         label="Ja"
@@ -49,12 +65,6 @@ const Omsorg: React.FunctionComponent<OmsorgProps> = props => {
         onChange={byttHarOmsorgen}
       />
     </RadioGruppe>
-    <Textarea
-      label={tekst.begrunnelse}
-      value={begrunnelse}
-      onChange={e => endreBegrunnelse(e.target.value)}
-      maxLength={0}
-    />
     <Hovedknapp>Bekreft og fortsett</Hovedknapp>
   </div>;
 };
