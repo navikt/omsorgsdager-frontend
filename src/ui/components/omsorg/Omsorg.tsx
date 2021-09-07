6ab07a2f1935e219a0d8619542a9060d38b8ecda
eras-dev <76997730+eras-dev@users.noreply.github.com>
2021-09-07 15:27:01 +0200

    ny versjon i dev (#314)

--- src/ui/components/omsorg/Omsorg.tsx
+++ src/ui/components/omsorg/Omsorg.tsx
@@ -50,7 +50,7 @@ const Omsorg: React.FunctionComponent<OmsorgProps> = ({
     instruksjon: `Vurder om søkeren har omsorgen for ${barnetEllerBarna}.`,
     sporsmalHarOmsorgen: `Har søker omsorgen for ${barnetEllerBarna}?`,
     begrunnelse: `Vurder om søker har omsorgen for ${barnetEllerBarna}`,
-    harBarnSoktForRammevedtakOmKroniskSykTekst: 'Det er allerede innvilget ekstra antall dager for dette barnet. Vurder om søknaden skal henlegges.',
+    harBarnSoktForRammevedtakOmKroniskSykTekst: 'Det er allerede innvilget ekstra antall dager for dette barnet. Vurder om søknaden skal henlegge.',
   };
   const tekst = {
     opplysningerFraSoknaden: 'Opplysninger fra søknaden:',
