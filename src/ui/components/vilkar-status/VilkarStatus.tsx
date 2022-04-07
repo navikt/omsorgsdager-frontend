7bcd73c9171f94403e45a1882fdf071f62f0e068
eras-dev <emma.rasmusson@nav.no>
2022-04-07 09:58:50 +0200

    revert Feat/automatisk innvilget ao omsorgen for (#596)

--- src/ui/components/vilkar-status/VilkarStatus.tsx
+++ src/ui/components/vilkar-status/VilkarStatus.tsx
@@ -1,10 +1,10 @@
 import classNames from 'classnames';
-import React from 'react';
 import Feilikon from '../../icons/Feilikon';
-import SjekkIkon from '../../icons/SjekkIkon';
+import React from 'react';
+import styles from './vilkarStatus.less';
 import Suksessikon from '../../icons/Suksessikon';
 import styleLesemodus from '../lesemodus/lesemodusboks.less';
-import styles from './vilkarStatus.less';
+import SjekkIkon from '../../icons/SjekkIkon';
 
 interface OwnProps {
   aksjonspunktNavn: string;
@@ -38,16 +38,12 @@ const VilkarStatus: React.FunctionComponent<OwnProps> = ({
             : 'Vilkåret er ikke oppfylt'
           }
         </p>
-
         {erVilkaretForOmsorgenFor && vilkarOppfylt && <div className={styles.beskrivelseForOmsorgenForOppfyltVilkar}>
           <SjekkIkon/> <h4>{beskrivelseForOmsorgenFor}</h4>
         </div>}
+        <p className={styles.begrunnelseOverskrift}>Vurdering</p>
+        <p className={classNames(styleLesemodus.fritekst, styles.begrunnelse)}>{begrunnelse}</p>
 
-        {begrunnelse && <>
-          <p className={styles.begrunnelseOverskrift}>Vurdering</p>
-          <p className={classNames(styleLesemodus.fritekst, styles.begrunnelse)}>{begrunnelse}</p>
-        </>
-        }
       </>
     );
   }
