3bfe591c5e477a3d0fa87408cc002b2ba4858fce
eras-dev <76997730+eras-dev@users.noreply.github.com>
2022-04-25 14:04:02 +0200

    Oppryddning (#627)
    * Revert "lagt in period for vilkarsstatus (#608)"
    
    This reverts commit 793817a5aeb9609628022e53ecc62ffe5087c235.
    
    * fjerner ting som bruktes av tidigare pipeline
    
    også lagt in infromasjon for att kunna göra repot åpent

--- webpack/webpack-config.production.js
+++ webpack/webpack-config.production.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -6,20 +5,22 @@ const commonWebpackConfig = require('./webpack.common.js');
 const pkg = require('./../package.json');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = env => {
-    return merge(commonWebpackConfig, {
-        mode: 'production',
-        output: {
-            filename: 'app.js',
-            path: path.resolve(__dirname, `../build/${env.buildToEnv}`),
-        },
-        optimization: {
-            minimize: true,
-            minimizer: [new TerserPlugin({extractComments: false}), new CssMinimizerPlugin()],
-        },
-        performance: {
-            maxAssetSize: 400000,
-            maxEntrypointSize: 600000,
-        },
-    });
-}
+const version = pkg.version;
+const versionSegments = version.split('.');
+const majorVersion = versionSegments[0];
+
+module.exports = merge(commonWebpackConfig, {
+    mode: 'production',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, `../build/${majorVersion}`),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({ extractComments: false }), new CssMinimizerPlugin()],
+    },
+    performance: {
+        maxAssetSize: 400000,
+        maxEntrypointSize: 600000,
+    },
+});
