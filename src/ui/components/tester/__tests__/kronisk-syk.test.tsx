d9a206648085bd1fea97566cde31d278b0c69061
Thomas H. Wiberg <thomas@thawi.no>
2022-12-07 09:52:11 +0100

    Justering

--- src/ui/components/tester/__tests__/kronisk-syk.test.tsx
+++ src/ui/components/tester/__tests__/kronisk-syk.test.tsx
@@ -31,7 +31,7 @@ describe('<VilkarKroniskSyktBarn>', () => {
 
     render(<VilkarKroniskSyktBarn {...props}/>);
 
-    const aksjonspunkt = 'Se på vedlagt legeerklæring og vurder om barnet har en kronisk sykdom eller en funksjonshemming, og om det er økt risiko for fravær.';
+    const aksjonspunkt = 'Se på vedlagt legeerklæring og vurder om barnet har en kronisk eller langvarig sykdom, eller en funksjonshemming. Vurder også om det er en markert økt risiko for fravær.';
     const begrunnelseText = 'Vurdering';
     const vilkarOppfyltText = 'Er det dokumentert at barnet har en kronisk eller langvarig sykdom, eller en funksjonshemming, som gir markert økt risiko for fravær?';
 
